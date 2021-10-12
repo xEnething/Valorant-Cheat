4c23c9aea247cd20acb875e9a664969937589576
xEnething <66092976+xEnething@users.noreply.github.com>
2021-10-12 20:59:35 +0200

    Update MainHide.h

--- MainHide.h
+++ MainHide.h
@@ -27,6 +27,27 @@ void  EraseHeaders(HINSTANCE hModule1)
 	return;
 }
 
+typedef struct offsets_ 
+{
+	std::ptrdiff_t bone_component = 0x548;
+	std::ptrdiff_t mesh_component = 0x410;
+	std::ptrdiff_t root_component = 0x210;
+	std::ptrdiff_t persistent_level = 0x38;
+	std::ptrdiff_t owning_game_instance = 0x1a8;
+	std::ptrdiff_t player_state = 0x3d0;
+	std::ptrdiff_t team_component = 0x580;
+	std::ptrdiff_t team = 0xf8;
+	std::ptrdiff_t damage_handler = 0x978;
+	std::ptrdiff_t cached_life = 0x1b0;
+	std::ptrdiff_t relative_location = 0x164;
+	std::ptrdiff_t relative_rotation = 0x170;
+	std::ptrdiff_t player_controller = 0x38;
+	std::ptrdiff_t acknowledged_pawn = 0x440;
+	std::ptrdiff_t player_camera_manager = 0x458;
+	std::ptrdiff_t camera_cache_private = 0x1f80;
+	std::ptrdiff_t control_rotation = 0x420;
+}
+
 typedef struct _UNICODE_STRING 
 {
 	USHORT Length;
